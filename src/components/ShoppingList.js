2672b000ac1d1d919ff03fa51376dba1ae9f6f20
Julien Hnery <jwh_917@yahoo.com>
2022-07-30 19:06:41 -0400

    Updated Components

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,25 +1,77 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+// function ShoppingList({ items }) {
+//   const [selectedCategory, setSelectedCategory] = useState("All");
 
-  function handleCategoryChange(event) {
-    // event.target.value will be the value selected by the user
-    setSelectedCategory(event.target.value);
-  }
+//   function handleCategoryChange(event) {
+//     // event.target.value will be the value selected by the user
+//     setSelectedCategory(event.target.value);
+//   }
+
+//   // we want to filter the items to only display the ones based on the selected category
+//   const itemsToDisplay = items.filter((item) => {
+//     if (selectedCategory === "All") return true;
+
+//     return item.category === selectedCategory;
+//   });
+
+//   return (
+//     <div className="ShoppingList">
+//       <div className="Filter">
+//         <select name="filter" onChange={handleCategoryChange}>
+//           <option value="All">Filter by category</option>
+//           <option value="Produce">Produce</option>
+//           <option value="Dairy">Dairy</option>
+//           <option value="Dessert">Dessert</option>
+//         </select>
+//       </div>
+//       <ul className="Items">
+//         {itemsToDisplay.map((item) => (
+//           <Item key={item.id} name={item.name} category={item.category} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default ShoppingList;
+
+// import React from "react";
+
+class ShoppingList extends React.Component {
+
+  state = {
+    selectedCategory: "All"
+  };
+
+  handleCategoryChange = (event) => {
+    this.setState((prevState) => ({
+      selectedCategory: prevState.selectedCategory = event.target.value
+    }));
+  };
 
   // we want to filter the items to only display the ones based on the selected category
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  // const itemsToDisplay = items.filter((item) => {
+  //   if (selectedCategory === "All") return true;
 
-    return item.category === selectedCategory;
-  });
+  //   return item.category === selectedCategory;
+  // });
+
+  render() {
+  const itemsToDisplay = this.props.items
+    .filter((item) => {
+      if (this.state.selectedCategory === "All") return true;
+
+      return item.category === this.state.selectedCategory;
+    })
+    
+
 
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
+        <select name="filter" onChange={this.handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -32,7 +84,8 @@ function ShoppingList({ items }) {
         ))}
       </ul>
     </div>
-  );
+    )
+  }
 }
 
 export default ShoppingList;
